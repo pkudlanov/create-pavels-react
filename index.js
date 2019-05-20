42f6bf7fea2b5e9bd651026d96082796cc08945e
Ryan Mehta <ryan.mehta@gmail.com>
2019-05-20 11:03:36 -0700

    historyApiFallback

--- index.js
+++ index.js
@@ -122,7 +122,8 @@ module.exports = {
     filename: 'bundle.[hash].js'
   },
   devServer: {
-    port: 7890
+    port: 7890,
+    historyApiFallback: true
   },
   plugins: [
     new HtmlPlugin({ template: './src/index.html' }),
